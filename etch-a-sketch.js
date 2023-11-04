f105ebcc7f4dc6fcf6d3798c73a58d191513f09d
ewarehi1 <128170875+ewarehi1@users.noreply.github.com>
2023-11-04 16:40:56 -0600

    Center items on page

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,7 +5,7 @@ document.body.appendChild(canvas);
 for ( let i = 1; i <= 16; i++ ) {
     const row = document.createElement('div');
     row.classList.add('row');
-    row.style.cssText = 'display: flex;';
+    row.style.cssText = 'display: flex; justify-content: center;';
     canvas.appendChild(row);
 
     for ( let i = 1; i <= 16; i++ ) {
@@ -13,13 +13,15 @@ for ( let i = 1; i <= 16; i++ ) {
         panel.classList.add('panel');
         panel.style.cssText = 'border: 2px solid black; height: 50px; width: 50px;';
         row.appendChild(panel);
-    }
-}
+    };
+};
 
 const panels = document.querySelectorAll('.panel');
 panels.forEach((panel) => {
     
     panel.addEventListener('mouseover', () => {
-        panel.style.cssText = 'border: 2px solid black; height: 50px; width: 50px; background-color: black;'
-    })
-})
+        panel.style.cssText = 'border: 2px solid black; height: 50px; width: 50px; background-color: black;';
+    });
+});
+
+document.body.style.cssText = 'height: 100%; width: 100%; position: absolute; display: flex; align-items: center; justify-content: center;';
