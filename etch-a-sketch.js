f58ec419646a72da5ea22c29b9c3032c006a2022
ewarehi1 <128170875+ewarehi1@users.noreply.github.com>
2023-11-04 15:18:37 -0600

    Create 16x16 canvas

---
+++ etch-a-sketch.js
@@ -0,0 +1,17 @@
+const canvas = document.createElement('div');
+canvas.setAttribute('id', 'canvas');
+document.body.appendChild(canvas);
+
+for ( let i = 1; i <= 16; i++ ) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    row.style.cssText = 'display: flex;';
+    canvas.appendChild(row);
+
+    for ( let i = 1; i <= 16; i++ ) {
+        const panel = document.createElement('div')
+        panel.classList.add('panel');
+        panel.style.cssText = 'border: 2px solid black; height: 50px; width: 50px;';
+        row.appendChild(panel);
+    }
+}
